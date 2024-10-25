3bd6fba58af57e17a208021d935b722bf87c8f21
devgauravjatt <devgauravjatt@gmail.com>
2024-10-25 06:59:19 +0530

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<div class="h-screen w-full">
+	<main class="mx-auto max-w-3xl flex justify-center items-center flex-col pt-20">
+		{@render children()}
+	</main>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Svg from '../lib/assets/circle.svg';
+	let colors = $state(['#BBFF00', '#06F586', '#ff3e00', '#8D462E', '#FF0037']);
+
+	let fillings = $state(0);
+
+	$effect(() => {
+		console.log(fillings);
+	});
+</script>
+
+<div>
+	<div class="flex gap-2 mb-4">
+		{#each colors as color, index}
+			<div class="flex flex-col gap-2">
+				<button
+					onclick={() => (fillings = index)}
+					style:background={colors[index]}
+					class="w-24 h-24 mb-3 rounded-full"
+				>
+					{#if index === fillings}
+						<img src={Svg} alt={index.toString()} />
+					{/if}
+				</button>
+				<span>
+					<code>
+						{colors[index]}
+					</code>
+				</span>
+			</div>
+		{/each}
+	</div>
+	<input bind:value={colors[fillings]} type="color" name="color" />
+</div>
